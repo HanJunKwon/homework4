13bd72cf4f9172c743336cbe75c5d6bfc2dfb35d
권한준 <skclaqks11@naver.com>
2016-10-18 22:16:20 +0900

    한글깨짐 수정 및 for문  수정

--- homework4/src/homework4/GuGuDan2.java
+++ homework4/src/homework4/GuGuDan2.java
@@ -5,29 +5,20 @@ public class GuGuDan2 {
 	
 	void guguCalc(){
 		String s;
-		System.out.println("###Â¦¼ö´Ü È¦¼ö´Ü Ãâ·ÂÇÏ±â");
 		Scanner sc = new Scanner(System.in);
 		
 		while(true){
-			System.out.print("E:Â¦¼ö´Ü, O:È¦¼ö´Ü(Q:Á¾·á) >> ");
+			System.out.print("E:ì§ìˆ˜ë‹¨, O:í™€ìˆ˜ë‹¨(Q:ì¢…ë£Œ) >> ");
 			s = sc.nextLine();
-			
-			if(s.equals("E")){
-				for(int i=1; i<10; ++i){
-					for(int j=2; j<10; j+=2)
-						System.out.printf("%d * %d = %d    ", j , i , j*i);
-					System.out.println();
-				}
-			}
-			else if(s.equals("O")){
-				for(int i=1; i<10; ++i){
-					for(int j=3; j<10; j+=2)
-						System.out.printf("%d * %d = %d    ", j, i, j*i);
-					System.out.println();
-				}
-			}
-			else if(s.equals("Q"))
+			if(s.equals("Q"))
 				break;
+			
+			int i =2;
+			if(s.equals("O")) i =3;
+			for(; i<10; i+=2){
+				for(int j=1; j<10; ++j)
+					System.out.printf("%d * %d = %d\n",i,j,i*j);
+			}
 		}
 	}
 }
